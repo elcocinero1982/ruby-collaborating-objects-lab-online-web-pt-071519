8d6466f7211968205eb5ec2d60cc375e7951f922
jose luis <cortezluisjose@gmail.com>
2019-08-19 02:16:28 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,20 +1,17 @@
-class Song 
-  attr_accessor :name, :artist 
-  @@all 
-  
-  def initialize(name, artist)
-    @@all=[]
-    
-  end 
-  def Song.all 
-    @@all 
-    
+
+class Song
+
+  attr_accessor :name, :artist
+
+  def initialize(name)
+    @name = name
   end
-  
-  
-  
-  
-  
-  
-  
-end
+
+  def self.new_by_filename(file)
+    song_name = file.split(" - ")[1]
+    artist = file.split(" - ")[0]
+    song = self.new(song_name)
+    
+    song.artist_name = artist
+    song
+  end
