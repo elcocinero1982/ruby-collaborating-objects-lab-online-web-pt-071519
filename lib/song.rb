a409aa0854bd629aca9c86d482b956908c11d608
jose luis <cortezluisjose@gmail.com>
2019-08-19 02:18:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -15,3 +15,10 @@ class Song
     song.artist_name = artist
     song
   end
+def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+   
+    self.artist.add_song(self)
+  end
+
+end
